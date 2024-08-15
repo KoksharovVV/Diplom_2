200b9604fc2a1519509e901fd9f288d000faead3
KoksharovVV <vladimircoksharoff@yandex.ru>
2024-08-15 19:55:08 +0500

    create conftest and upd requirements

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import requests
+from faker import Faker
+from data import TestDataUrl
+# from helpers.helpers import CreateUser
+
+
+@pytest.fixture
+def fake_user():
+    fake = Faker()
+    fake_user = {
+        "email": fake.email(),
+        "password": fake.password(),
+        "name": fake.user_name()
+    }
+    return fake_user
+@pytest.fixture
+def create_user_delete_user(fake_user):
+    response = requests.post(url=TestDataUrl.CREATE_USER_URL, data=fake_user)
+    access_token = response.json()["accessToken"]
+    yield response, fake_user, access_token
+    requests.delete(url=TestDataUrl.AUTH_USER_URL, headers={'Authorization': access_token})
