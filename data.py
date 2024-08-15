d3659c81a17a0576c0d2053448145b2d31ed0874
KoksharovVV <vladimircoksharoff@yandex.ru>
2024-08-15 19:05:58 +0500

    create structure

---
+++ data.py
@@ -0,0 +1,5 @@
+class TestDataUrl:
+    CREATE_USER_URL = "https://stellarburgers.nomoreparties.site/api/auth/register"
+    AUTHORIZATION_URL = "https://stellarburgers.nomoreparties.site/api/auth/login"
+    AUTH_USER_URL = "https://stellarburgers.nomoreparties.site/api/auth/user"
+    CREATE_ORDER_URL = "https://stellarburgers.nomoreparties.site/api/orders"
